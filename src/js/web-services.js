9030885c8b1fa9249d1b0b2b75c948579160f3f6
mtbotherd <mtbotherd@users.noreply.github.com>
2018-12-28 11:35:36 -0600

    Updates.

--- src/js/web-services.js
+++ src/js/web-services.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         });
     });
 
-    // Get Providers
+    // Get Routes
     $('#jsGetRoutes').click(function() {
         var routes = $('.routes');
 
@@ -40,9 +40,55 @@ $(document).ready(function() {
                 for (var i in data) {
                     output += '<option>' + data[i].Description + '</option>';
                 }
-                //output += '</option>';
+                output += '</select></div>';
                 routes.html(output);
                 $('#jsGetRoutes').hide();
+            }
+        });
+    });
+
+    // Get Directions
+    $('#jsGetDirections').click(function() {
+        var directions = $('.directions');
+
+        directions.text('Loading data from JSON source...');
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://svc.metrotransit.org/NexTrip/Directions/5?format=json',
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                var output = '<p>';
+                for (var i in data) {
+                    output += data[i].Text + '<br />';
+                }
+                output += '</p>';
+                directions.html(output);
+                $('#jsGetDirections').hide();
+            }
+        });
+    });
+
+    // Get Stops
+    $('#jsGetStops').click(function() {
+        var stops = $('.stops');
+
+        stops.text('Loading data from JSON source...');
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://svc.metrotransit.org/NexTrip/Stops/5/4?format=json',
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                var output = '<p>';
+                for (var i in data) {
+                    output += data[i].Text + '-' + data[i].Value + '<br />';
+                }
+                output += '</p>';
+                stops.html(output);
+                $('#jsGetStops').hide();
             }
         });
     });
